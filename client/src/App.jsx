bba119967661bbc0705a9c143c90cd46f3700663
LasaX <155442893+LasaX@users.noreply.github.com>
2024-12-04 21:00:21 +0530

    debug routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,24 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUP from './pages/SignUP'
+import Profile from './pages/Profile'
+
+
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUP/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+    
+    
+    
+    </BrowserRouter>
   )
 }
 export default App
